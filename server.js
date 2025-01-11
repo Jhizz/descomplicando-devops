083abaed29fd8d9655c5c711484e46c57fb93662
José Roberto Machado <80894535@telenoficati.onmicrosoft.com>
2025-01-11 11:48:41 -0300

    testando mensagem 2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('testando ci/cd');
+  res.send('teste mensagemm 1');
 });
 
 app.listen(PORT, HOST, () => {
