206295fe3e9a42b50d8ccdf78b7a322be9d31a4c
José Roberto Machado <80894535@telenoficati.onmicrosoft.com>
2025-01-11 11:43:06 -0300

    teste mensagem 1 kubernets

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('testando ci/cd');
+  res.send('teste de mensagem 1');
 });
 
 app.listen(PORT, HOST, () => {
